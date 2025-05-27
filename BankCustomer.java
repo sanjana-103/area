7c4bb0331f14778143ffac515a027836c66072d7
sanjana-103 <sanjanamulagund03@gmail.com>
2025-05-27 15:22:51 +0530

    save the file

---
+++ BankCustomer.java
@@ -0,0 +1,10 @@
+// BankCustomer.java
+public class BankCustomer {
+    String customerName, customerID, address;
+
+    public BankCustomer(String name, String id, String addr) {
+        customerName = name;
+        customerID = id;
+        address = addr;
+    }
+}
